ce32807b62eb55e2eabfa3b69aec2b86276b500a
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-02-02 18:57:19 -0800

    flexible email validation

--- js/HelperClass.js
+++ js/HelperClass.js
@@ -1,6 +1,6 @@
 class HelperClass {
   static validateEmail = email => {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
   };
 }
